e11a19aeabfb022f792b9decaacd3527912f7486
Sparin <sparin285@gmail.com>
2017-02-28 10:14:46 +0300

    Full solution

---
+++ W01E02-a+pow(b,2)/src/W01E02-a+pow(b,2)/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("W01E02_a_pow_b_2_")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c5614cc7-18d0-4f7c-9bda-ec8a42db8ddc")]
