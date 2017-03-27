e83c1abbd5c4e47b2ee7e884569e32b8ba44498f
Sparin <sparin285@gmail.com>
2017-03-27 15:14:37 +0300

    Full solution

---
+++ W04E06-Postfix/W04E06-Postfix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace W04E06_Postfix
+{
+    class Program
+    {
+        private static Stack<int> stack = new Stack<int>();
+
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("output.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            Console.SetOut(sw);
+
+            string[] stdin = File.ReadAllLines("input.txt")[1].Split(' ');
+
+            int a, b;
+            for (int i = 0; i < stdin.Length; i++)
+            {
+                switch (stdin[i])
+                {
+                    case "+":
+                        a = stack.Pop();
+                        b = stack.Pop();
+                        stack.Push(a + b);
+                        break;
+                    case "-":
+                        a = stack.Pop();
+                        b = stack.Pop();
+                        stack.Push(b - a);
+                        break;
+                    case "*":
+                        a = stack.Pop();
+                        b = stack.Pop();
+                        stack.Push(b * a);
+                        break;
+                    default:
+                        stack.Push(int.Parse(stdin[i]));
+                        break;
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+
+            sw.Dispose();
+        }
+    }
+}
